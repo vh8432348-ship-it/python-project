4bb7e0eaea799e9943e889c69e5187b18cf81aed
vh8432348-ship-it <vh8432348@gmail.com>
2026-03-26 19:48:45 +0200

    updated works

--- main.py
+++ main.py
@@ -6,3 +6,18 @@ class Student:
 
     def show_info(self):
         print(f"Ім’я: {self.name}, вік: {self.age}")
+
+
+students = []
+
+for i in range(3):
+    name = input("Введіть ім’я: ")
+    age = int(input("Введіть вік: "))
+
+    s = Student(name, age)
+    students.append(s)
+
+print("Інформація про студентів:")
+
+for student in students:
+    student.show_info()
