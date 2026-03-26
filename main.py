94fdd22f7ba5d7d6f5fd50a066a258fa979e2352
vh8432348-ship-it <vh8432348@gmail.com>
2026-03-26 20:05:48 +0200

    updated works

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
+import math
+
 # Завдання 1
+
+
 class Student:
     def __init__(self, name, age):
         self.name = name
@@ -21,3 +25,19 @@ print("Інформація про студентів:")
 
 for student in students:
     student.show_info()
+# Завдання 3
+
+
+class Circle:
+    def __init__(self, radius):
+        self.radius = radius
+
+    def get_area(self):
+        area = math.pi * self.radius**2
+        return area
+
+
+c1 = Circle(5)
+
+print(c1.get_area())
+# Завдання 4
