921d5122a2687b3ced2bf9bff8ca7e43d4944956
vh8432348-ship-it <vh8432348@gmail.com>
2026-03-31 20:25:00 +0300

    updated code

--- main.py
+++ main.py
@@ -1,62 +1,38 @@
 # Завдання 1
-"""
-    Є текстовий файл. Запишіть в інший файл таку
-статистику:
- Кількість символів
- Кількість рядків
- Кількість цифр
- Кількість голосних літер(aeuio)
-"""
+class Project:
+    def __init__(
+        self, name, budget, total_cost=0, is_completed=False, duration=0, tasks=None
+    ):
+        self.name = name
+        self.budget = budget
+        self.total_cost = total_cost
+        self.is_completed = is_completed
+        self.duration = duration
+        self.tasks = tasks if tasks is not None else []
 
-numbers = "1234567890"
-numbers_count = 0
-vowel_letter = 0
-litters = "aeuio"
-with open("test.txt", "r") as file:
-    content = file.read()
-    count_sym = len(content)
-    count_of_lines = len(content.splitlines())
+    def show_info(self):
+        print(
+            f"Назва: {self.name} Час виконання: {self.duration} Необхідні задачі: {self.tasks}"
+        )
 
-    for a in content:
-        if a in numbers:
-            numbers_count += 1
-        if a in litters:
-            vowel_letter += 1
-print(
-    f"Кількість символів: {count_sym} Кількість рядків: {count_of_lines} "
-    f"Кількість цифр: {numbers_count} Кількість голосних літер: {vowel_letter}"
-)
-# Завдання 2
-"""
-    Користувач вводить слово та назву файлу. Виведіть
-кількість цього слова у файлі.
-"""
-name_of_file = input("Введіть назву файлу: ")
-target = input("Ввеідть слово яке потрібной знайти: ")
+    def add_task(self, task):
+        self.tasks.append(task)
 
-try:
-    with open(name_of_file, "r", encoding="utf-8") as file:
-        content = file.read()
+    def split_task(self, task_name, subtasks):
+        for task in self.tasks:
+            if task["name"] == task_name:
+                task["subtasks"] = subtasks
+                return
+        print("Задачу не знайдено")
 
-except FileNotFoundError:
-    print("Файл не знайдено, використовується test.txt")
-    with open("test.txt", "r", encoding="utf-8") as file:
-        content = file.read()
-        name_of_file = "test.txt"
+    def complete_task(self, task_name, time_spent, cost):
+        for task in self.tasks:
+            if task["name"] == task_name:
+                task["completed"] = True
+                task["time"] = time_spent
+                task["cost"] = cost
 
-words = content.split()
-result = words.count(target)
-
-print(f"Кількість цього слова {target} у файлі: {name_of_file} = {result}")
-# Завдання 3
-"""
-    Є текстовий файл. Видаліть з нього останній рядок
-"""
-with open("test.txt", "r", encoding="utf-8") as file:
-    lines = file.readlines()
-
-
-lines = lines[:-1]
-
-with open("test.txt", "w", encoding="utf-8") as file:
-    file.writelines(lines)
+                self.duration += time_spent
+                self.total_cost += cost
+                return
+        print("Задачу не знайдено")
