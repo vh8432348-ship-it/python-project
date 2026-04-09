e2a477ac50bdd62bedce9ab5abde134b4d0ca38a
vh8432348-ship-it <vh8432348@gmail.com>
2026-04-09 20:31:01 +0300

    fgdgdgdg

--- main.py
+++ main.py
@@ -1,144 +1,67 @@
-from datetime import datetime
-
-
 # Завдання 1
 
 
-class Message:
-    def __init__(self, user, text, time_str):
-        self.user = user
-        self.text = text
-        self.time = datetime.strptime(time_str, "%H:%M")
+class Character:
+    def __init__(
+        self,
+        name,
+        max_hp,
+        intelligence,
+        strength,
+        dexterity,
+        mana,
+        defense,
+        level=1,
+    ):
+        self._name = name
+        self._max_hp = max_hp
+        self._hp = max_hp
+        self._level = level
+        self._intelligence = intelligence
+        self._strength = strength
+        self._dexterity = dexterity
+        self._mana = mana
+        self._defense = defense
 
-    def __str__(self):
-        return f"{self.user}: {self.text} ({self.time.strftime('%H:%M')})"
+    def attack(self):
+        raise NotImplementedError("Метод attack потрібно перевизначити")
 
-    def __len__(self):
-        return len(self.text)
+    def take_damage(self, damage):
+        real_damage = damage - self._defense
+        if real_damage < 0:
+            real_damage = 0
 
-    def __gt__(self, other):
-        return self.time > other.time
+        self._hp -= real_damage
 
+        if self._hp < 0:
+            self._hp = 0
 
-messages = [
-    Message("Ivan", "Привіт", "10:23"),
-    Message("Oleg", "Як справи?", "09:15"),
-    Message("Anna", "Все добре!", "11:05"),
-]
+    def level_up(self):
+        if self._level < 20:
+            self._level += 1
 
-print("До сортування:")
-for mess in messages:
-    print(mess)
+    def increase_stat(self, stat):
+        if stat == "strength":
+            self._strength += 1
+        elif stat == "intelligence":
+            self._intelligence += 1
+        elif stat == "dexterity":
+            self._dexterity += 1
+        elif stat == "mana":
+            self._mana += 1
+        elif stat == "defense":
+            self._defense += 1
 
-messages.sort()
+    def rest(self):
+        self._hp = self._max_hp
 
-print("Після сортування:")
-for mess in messages:
-    print(mess)
+    def heal(self, heal_hp):
+        self._hp += heal_hp
+        if self._hp > self._max_hp:
+            self._hp = self._max_hp
+
+    def get_info(self):
+        return f"{self._name} | HP: {self._hp}/{self._max_hp} | Level: {self._level}"
 
 
 # Завдання 2
-
-
-class Song:
-    def __init__(self, name, author):
-        self.name = name
-        self.author = author
-
-    def __eq__(self, other):
-        if not isinstance(other, Song):
-            return False
-        return self.name == other.name and self.author == other.author
-
-    def __str__(self):
-        return f"{self.name} - {self.author}"
-
-
-class Playlist:
-    def __init__(self):
-        self.songs = []
-
-    def __len__(self):
-        return len(self.songs)
-
-    def __contains__(self, item):
-        if not isinstance(item, Song):
-            return False
-        return item in self.songs
-
-    def __iter__(self):
-        return iter(self.songs)
-
-    def add_song(self, song):
-        if not isinstance(song, Song):
-            raise TypeError("Можна додавати тільки об'єкти Song")
-        self.songs.append(song)
-
-    def remove_song(self, song):
-        if not isinstance(song, Song):
-            raise TypeError("Можна видаляти тільки об'єкти Song")
-        if song in self.songs:
-            self.songs.remove(song)
-
-
-playlist = Playlist()
-
-song1 = Song("Imagine", "John Lennon")
-song2 = Song("Bohemian Rhapsody", "Queen")
-song3 = Song("Shape of You", "Ed Sheeran")
-
-playlist.add_song(song1)
-playlist.add_song(song2)
-playlist.add_song(song3)
-
-for song in playlist:
-    print(song)
-
-
-# Завдання 3
-
-
-class Cart:
-    def __init__(self):
-        self.items = []
-        self.total = 0
-
-    def __str__(self):
-        return f"Товари: {', '.join(self.items)} | Сума: {self.total}"
-
-    def __len__(self):
-        return len(self.items)
-
-    def __add__(self, other):
-        if not isinstance(other, Cart):
-            raise TypeError("Можна об'єднувати тільки Cart")
-
-        new_cart = Cart()
-        new_cart.items = self.items + other.items
-        new_cart.total = self.total + other.total
-        return new_cart
-
-    def add_item(self, item, price):
-        self.items.append(item)
-        self.total += price
-
-
-cart1 = Cart()
-cart2 = Cart()
-
-cart1.add_item("Хліб", 20)
-cart1.add_item("Молоко", 30)
-
-cart2.add_item("Сир", 50)
-cart2.add_item("Яблука", 40)
-
-print(len(cart1))
-print(len(cart2))
-
-print(cart1)
-print(cart2)
-
-cart3 = cart1 + cart2
-
-print(len(cart3))
-print(cart3)
