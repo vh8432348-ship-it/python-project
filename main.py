4c4b045d853a5013e6fcc5e9e180912ae5b7fb23
vh8432348-ship-it <vh8432348@gmail.com>
2026-04-28 19:39:22 +0300

    fddfdf

--- main.py
+++ main.py
@@ -1,160 +1,95 @@
 import json
+import pickle
 
-FILE_NAME = "users.json"
+
+def add_product(products):
+    product = input("Введіть назву товару: ")
+    products.append(product)
+    print("Товар додано!")
 
 
-def load_users():
+def show_products(products):
+    if not products:
+        print("Список товарів порожній.")
+    else:
+        print("Список товарів:")
+        for i, product in enumerate(products, start=1):
+            print(f"{i}. {product}")
+
+
+def save_json(products, filename="products.json"):
+    with open(filename, "w", encoding="utf-8") as file:
+        json.dump(products, file, ensure_ascii=False, indent=4)
+    print("Дані збережено у JSON!")
+
+
+def load_json(filename="products.json"):
     try:
-        with open(FILE_NAME, "r", encoding="utf-8") as file:
-            return json.load(file)
+        with open(filename, "r", encoding="utf-8") as file:
+            products = json.load(file)
+        print("Дані завантажено з JSON!")
+        return products
     except FileNotFoundError:
-        return {}
-    except json.JSONDecodeError:
-        return {}
+        print("Файл JSON не знайдено.")
+        return []
 
 
-# Змінив save_users
-def save_users(users):
-    with open("users.json", "w", encoding="utf-8") as file:
-        json.dump(users, file, indent=4, ensure_ascii=False)
+def save_pickle(products, filename="products.pkl"):
+    with open(filename, "wb") as file:
+        pickle.dump(products, file)
+    print("Дані збережено у Pickle!")
 
 
-def add_user(users):
-    login = input("Логін: ")
-
-    if login in users:
-        print("Користувач вже існує!")
-        return
-
-    password = input("Пароль: ")
-    users[login] = password
-    print("Користувача додано")
-
-
-def delete_user(users):
-    login = input("Логін: ")
-
-    if login in users:
-        del users[login]
-        print("Користувача видалено")
-    else:
-        print("Не знайдено")
-
-
-def change_password(users):
-    login = input("Логін: ")
-
-    if login in users:
-        users[login] = input("Новий пароль: ")
-        print("Пароль змінено")
-    else:
-        print("Не знайдено")
-
-
-def login(users):
-    login = input("Логін: ")
-    password = input("Пароль: ")
-
-    if users.get(login) == password:
-        print("Вхід успішний!")
-    else:
-        print("Невірний логін або пароль")
+def load_pickle(filename="products.pkl"):
+    try:
+        with open(filename, "rb") as file:
+            products = pickle.load(file)
+        print("Дані завантажено з Pickle!")
+        return products
+    except FileNotFoundError:
+        print("Файл Pickle не знайдено.")
+        return []
 
 
 def menu():
-    users = load_users()
+    products = []
 
     while True:
-        print("\n1. Додати користувача")
-        print("2. Видалити користувача")
-        print("3. Змінити пароль")
-        print("4. Вхід")
-        print("5. Зберегти і вийти")
+        print("Меню:")
+        print("1. Додати товар")
+        print("2. Вивести список товарів")
+        print("3. Зберегти у JSON")
+        print("4. Зберегти у Pickle")
+        print("5. Завантажити з JSON")
+        print("6. Завантажити з Pickle")
+        print("7. Вийти")
 
-        choice = input("Вибір: ")
+        choice = input("Ваш вибір: ")
 
         if choice == "1":
-            add_user(users)
+            add_product(products)
 
         elif choice == "2":
-            delete_user(users)
+            show_products(products)
 
         elif choice == "3":
-            change_password(users)
+            save_json(products)
 
         elif choice == "4":
-            login(users)
+            save_pickle(products)
 
         elif choice == "5":
-            save_users(users)
-            print("Збережено. Вихід...")
+            products = load_json()
+
+        elif choice == "6":
+            products = load_pickle()
+
+        elif choice == "7":
+            print("Програму завершено.")
             break
 
         else:
-            print("Невірний вибір")
+            print("Невірний вибір. Спробуйте ще раз.")
 
 
 menu()
-
-# Завдання 2
-
-
-class Cart:
-    def __init__(self, user):
-        self._user = user
-        self._items = []
-        self._total = 0
-
-    def add(self, item, price):
-        self._items.append({"item": item, "price": price})
-        self._total += price
-
-    def delete(self, item, price):
-        for i in self._items:
-            if i["item"] == item and i["price"] == price:
-                self._items.remove(i)
-                self._total -= price
-                break
-
-    def info(self):
-        print(f"User: {self._user}")
-        print("Items:")
-        for i in self._items:
-            print(f"- {i['item']} : {i['price']}")
-        print(f"Total: {self._total}")
-
-    def save(self, filename="cart.json"):
-        data = {"user": self._user, "items": self._items, "total": self._total}
-
-        with open(filename, "w", encoding="utf-8") as file:
-            json.dump(data, file, indent=4, ensure_ascii=False)
-
-    def load(self, filename="cart.json"):
-        try:
-            with open(filename, "r", encoding="utf-8") as file:
-                data = json.load(file)
-
-                self._user = data.get("user")
-                self._items = data.get("items", [])
-                self._total = data.get("total", 0)
-
-        except FileNotFoundError:
-            print("Файл не знайдено")
-
-
-# Завдання 3
-
-with open("settings.json", "r", encoding="utf-8") as file:
-    settings = json.load(file)
-
-size = settings.get("size")
-background_color = settings.get("background_color")
-button_color = settings.get("button_color")
-button_position = settings.get("button_position")
-instruction = settings.get("instruction")
-
-print(size)
-print(background_color)
-print(button_color)
-print(button_position)
-print(instruction)
