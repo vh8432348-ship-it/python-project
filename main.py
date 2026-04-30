e6ecd0e211c218db2c5b165861f2a46ea7bb41f5
vh8432348-ship-it <vh8432348@gmail.com>
2026-04-30 20:12:14 +0300

    dggdf

--- main.py
+++ main.py
@@ -2,23 +2,53 @@ import threading
 
 
 # Завдання 1
-def find_max(numbers):
-    maximum = max(numbers)
-    print(f"Максимум у списку: {maximum}")
 
 
-def find_min(numbers):
-    minimum = min(numbers)
-    print(f"Мінімум у списку: {minimum}")
+def find_max(numbers, res_max):
+    res_max["max"] = max(numbers)
+
+
+def find_min(numbers, res_min):
+    res_min["min"] = min(numbers)
 
 
 numbers = list(map(int, input("Введіть числа через пробіл: ").split()))
 
-thread_max = threading.Thread(target=find_max, args=(numbers,))
-thread_min = threading.Thread(target=find_min, args=(numbers,))
+res_max: dict[str, int] = {}
+res_min: dict[str, int] = {}
+
+thread_max = threading.Thread(target=find_max, args=(numbers, res_max))
+thread_min = threading.Thread(target=find_min, args=(numbers, res_min))
 
 thread_max.start()
 thread_min.start()
 
 thread_max.join()
 thread_min.join()
+
+print(f"Максимум у списку: {res_max['max']}")
+print(f"Мінімум у списку: {res_min['min']}")
+
+# Завдання 2
+
+
+def find_sum(numbers):
+    total = sum(numbers)
+    print(f"Сума елементів списку: {total}")
+
+
+def find_average(numbers):
+    average = sum(numbers) / len(numbers)
+    print(f"Середнє арифметичне: {average}")
+
+
+numbers = list(map(int, input("Введіть числа через пробіл: ").split()))
+
+thread_sum = threading.Thread(target=find_sum, args=(numbers,))
+thread_average = threading.Thread(target=find_average, args=(numbers,))
+
+thread_sum.start()
+thread_average.start()
+
+thread_sum.join()
+thread_average.join()
