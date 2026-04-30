e0309f7b552d3a55c9a55b0c81454b423d5af550
vh8432348-ship-it <vh8432348@gmail.com>
2026-04-30 20:48:59 +0300

    dff

--- main.py
+++ main.py
@@ -64,3 +64,52 @@ thread_average.start()
 
 thread_sum.join()
 thread_average.join()
+
+# Завдання 3
+
+
+def write_even_numbers(numbers: list[int], result: dict[str, int]):
+    even_numbers = [num for num in numbers if num % 2 == 0]
+
+    with open("even_numbers.txt", "w", encoding="utf-8") as file:
+        for num in even_numbers:
+            file.write(f"{num}\n")
+
+    result["even_count"] = len(even_numbers)
+
+
+def write_odd_numbers(numbers: list[int], result: dict[str, int]):
+    odd_numbers = [num for num in numbers if num % 2 != 0]
+
+    with open("odd_numbers.txt", "w", encoding="utf-8") as file:
+        for num in odd_numbers:
+            file.write(f"{num}\n")
+
+    result["odd_count"] = len(odd_numbers)
+
+
+file_path = input("Введіть шлях до файлу з числами: ")
+
+with open(file_path, "r", encoding="utf-8") as file:
+    numbers = list(map(int, file.read().split()))
+
+result0: dict[str, int] = {}
+
+thread_even = threading.Thread(
+    target=write_even_numbers,
+    args=(numbers, result0),
+)
+
+thread_odd = threading.Thread(
+    target=write_odd_numbers,
+    args=(numbers, result0),
+)
+
+thread_even.start()
+thread_odd.start()
+
+thread_even.join()
+thread_odd.join()
+
+print(f"Кількість парних чисел: {result0['even_count']}")
+print(f"Кількість непарних чисел: {result0['odd_count']}")
