1ef894137a01af968af416902ea930ce27b240ad
vh8432348-ship-it <vh8432348@gmail.com>
2026-06-07 18:41:38 +0300

    dgdf

--- docker_films/main.py
+++ docker_films/main.py
@@ -1,10 +1,5 @@
 import uvicorn
-
 from settings import settings
 
 if __name__ == "__main__":
-    uvicorn.run(
-        "application:app",
-        host=settings.host,
-        port=settings.port,
-    )
+    uvicorn.run("application:app", host=settings.host, port=settings.port, reload=True)
